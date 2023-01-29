59c7c76b2cf83cc5c2b7954d34ea29991535c048
Anton Ždanov <anton@azdanov.dev>
2023-01-29 20:50:20 +0200

    add events

--- tests/steps/when.js
+++ tests/steps/when.js
@@ -2,6 +2,7 @@ const _ = require("lodash");
 const aws4 = require("aws4");
 const URL = require("node:url");
 const http = require("axios");
+const EventBridge = require("aws-sdk/clients/eventbridge");
 
 const APP_ROOT = "../../";
 
@@ -17,7 +18,7 @@ const viaHandler = async (event, functionName) => {
     "headers.content-type",
     "application/json"
   );
-  if (response.body && contentType === "application/json") {
+  if (_.get(response, "body") && contentType === "application/json") {
     response.body = JSON.parse(response.body);
   }
   return response;
@@ -77,6 +78,22 @@ const viaHttp = async (relPath, method, opts) => {
   }
 };
 
+const viaEventBridge = async (busName, source, detailType, detail) => {
+  const eventBridge = new EventBridge();
+  await eventBridge
+    .putEvents({
+      Entries: [
+        {
+          Source: source,
+          DetailType: detailType,
+          Detail: JSON.stringify(detail),
+          EventBusName: busName,
+        },
+      ],
+    })
+    .promise();
+};
+
 const we_invoke_get_index = async () => {
   switch (mode) {
     case "handler": {
@@ -122,8 +139,41 @@ const we_invoke_search_restaurants = async (theme, user) => {
   }
 };
 
+const we_invoke_place_order = async (user, restaurantName) => {
+  const body = JSON.stringify({ restaurantName });
+
+  switch (mode) {
+    case "handler": {
+      return await viaHandler({ body }, "place-order");
+    }
+    case "http": {
+      const auth = user.idToken;
+      return await viaHttp("orders", "POST", { body, auth });
+    }
+    default: {
+      throw new Error(`unsupported mode: ${mode}`);
+    }
+  }
+};
+
+const we_invoke_notify_restaurant = async (event) => {
+  if (mode === "handler") {
+    await viaHandler(event, "notify-restaurant");
+  } else {
+    const busName = process.env.bus_name;
+    await viaEventBridge(
+      busName,
+      event.source,
+      event["detail-type"],
+      event.detail
+    );
+  }
+};
+
 module.exports = {
   we_invoke_get_index,
   we_invoke_get_restaurants,
   we_invoke_search_restaurants,
+  we_invoke_place_order,
+  we_invoke_notify_restaurant,
 };
